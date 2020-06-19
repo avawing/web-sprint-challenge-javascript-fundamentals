807dc006d55663af956f3be9178bdce46dd6ac58
Ava Wingfield <avelinewingfield@gmail.com>
2020-06-19 11:28:42 -0400

    Plech.

--- challenges/arrays-callbacks.js
+++ challenges/arrays-callbacks.js
@@ -49,10 +49,16 @@ console.log(lowPopulationAnimals);
 
 /* Request 4: .reduce() 
 
-The zoos need to know their total animal population across the United States. Find the total population from all the zoos using the .reduce() method. Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
+
+array.reduce(function(total, currentValue, currentIndex, arr), initialValue)
+The zoos need to know their total animal population across the United States. 
+Find the total population from all the zoos using the .reduce() method. 
+Remember the reduce method takes two arguments: a callback (which itself takes two args), and an initial value for the count.
 
 */
-let populationTotal = 0;
+let populationTotal = zooAnimals.map(item => item.population).reduce((function(accumulator, next){return accumulator+next}))
+
+
 console.log(populationTotal);
 
 
